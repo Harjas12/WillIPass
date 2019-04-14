64985083896f1e3921d2ff7772dd7f4a621bd259
Harjas Monga <harjasmonga@gmail.com>
2019-04-14 19:23:08 -0400

    added cors support

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const app = express();
 
 // app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 app.use('/api', api);
 
 const port = process.env.PORT || 3000;

8ba2aeb5d6616a5df6d5166176bd7c5d753f9820
Harjas Monga <harjasmonga@gmail.com>
2019-04-14 18:01:13 -0400

    added authentication, login, and create

--- backend/api.js
+++ backend/api.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+
 const express = require('express');
 const pg = require('pg');
+const encrypter = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 const dbConn = new pg.Pool({
@@ -10,14 +14,72 @@ const dbConn = new pg.Pool({
 	port: process.env.DB_PORT
 });
 
+function checkAuth(req, res, next) {
+	let token = req.headers['authorization'];
+	console.log(req.headers);
+	if(!token) {
+		res.status(401).send({ auth: false, message: 'No token provided.' });
+		return;
+	}
+	token = token.substring(7);
+	try {
+		let jwtToken = jwt.verify(token, process.env.SECRET);
+		req.user_id = jwtToken.id;
+		next();
+	} catch(error) {
+		res.sendStatus(401);
+		return;
+	}
+}
+
+router.get('/test', checkAuth, (req, res) => {
+	res.send("HELLO");
+});
+
 router.post('/create', async (req, res) => {
 	let username = req.body.username;
 	let password = req.body.password;
 	if(!username || !password) {
-		res.send(400);
+		res.sendStatus(400);
 		return;
 	}
-	res.send("test");
+	let salt = await encrypter.genSalt();
+	let hash = await encrypter.hash(password, salt);
+	try {
+		let results = await dbConn.query("INSERT INTO account (username, password, salt) VALUES ($1, $2, $3)",
+		[username, hash, salt]);
+		res.sendStatus(201);
+	} catch(error) {
+		console.log(error);
+		res.sendStatus(400);
+	}
+});
+
+router.post('/login', async (req, res) => {
+	let username = req.body.username;
+	let password = req.body.password;
+	if(!username || !password) {
+		res.sendStatus(400);
+		return;
+	}
+	try {
+		let queryRes = await dbConn.query("SELECT password, salt, user_id FROM account WHERE username = $1",
+		[username]);
+		if(queryRes.rowCount != 1) {
+			res.sendStatus(401);
+			return;
+		}
+		let authInfo = queryRes.rows[0];
+		let recievedHash = await encrypter.hash(password, authInfo.salt);
+		if(recievedHash == authInfo.password) {
+			let token = jwt.sign({id: authInfo.user_id}, process.env.SECRET);
+			res.send({auth: true, token: token});
+		} else {
+			res.sendStatus(401);
+		}
+	} catch(error) {
+		res.sendStatus(401);
+	}
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
-require('dotenv').config();
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const api = require('./api');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use('/api', api);
 
-app.listen(process.env.PORT || 3000);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`listening on port ${port}`)
+});

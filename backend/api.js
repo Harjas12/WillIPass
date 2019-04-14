1908227778a1f8d18e21584241b2123487924df0
Harjas Monga <harjasmonga@gmail.com>
2019-04-14 15:10:33 -0400

    setup basics of db and hooked up server to db

--- backend/api.js
+++ backend/api.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const pg = require('pg');
 const router = express.Router();
 
+const dbConn = new pg.Pool({
+	user: process.env.DB_USER,
+	host: process.env.DB_HOST,
+	database: process.env.DB_NAME,
+	password: process.env.DB_PASS,
+	port: process.env.DB_PORT
+});
 
-router.get('/', (req, res) => {
+router.post('/create', async (req, res) => {
+	let username = req.body.username;
+	let password = req.body.password;
+	if(!username || !password) {
+		res.send(400);
+		return;
+	}
 	res.send("test");
 });
 

6399177b97ead1109433ea029d8cd1996b1073af
jharesco <jharesco@purdue.edu>
2019-04-14 17:54:37 -0400

    made calculator component

--- gpa-calc-app/src/app/app-routing.module.ts
+++ gpa-calc-app/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+import { calculatorComponent } from "./calculatorComponent/calculatorComponent.component";
+
+const routes: Routes = [{ path: "", component: calculatorComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

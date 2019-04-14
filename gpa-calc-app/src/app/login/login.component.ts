d1947281bdc1c9457d11041a9f4594bc18312614
jharesco <jharesco@purdue.edu>
2019-04-14 17:44:34 -0400

    created login form and able to store values entered

--- gpa-calc-app/src/app/login/login.component.ts
+++ gpa-calc-app/src/app/login/login.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
+  newLoginForm: FormGroup;
+  username: string = "";
+  password: string = "";
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
+    this.newLoginForm = this.fb.group({
+      username: ["", Validators.required],
+      password: ["", Validators.required]
+    });
   }
 
+  onLogin() {
+    this.username = this.newLoginForm.get("username").value;
+    this.password = this.newLoginForm.get("password").value;
+  }
+
+  ngOnInit() {}
 }

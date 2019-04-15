55a9b18e3219110995a004c3810418aca313b745
jharesco <jharesco@purdue.edu>
2019-04-15 02:36:30 -0400

    Dynamic form for class and grades created

--- gpa-calc-app/src/app/calculator-page/calculator-page.component.ts
+++ gpa-calc-app/src/app/calculator-page/calculator-page.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-calculator-page',
-  templateUrl: './calculator-page.component.html',
-  styleUrls: ['./calculator-page.component.css']
+  selector: "app-calculator-page",
+  templateUrl: "./calculator-page.component.html",
+  styleUrls: ["./calculator-page.component.css"]
 })
 export class CalculatorPageComponent implements OnInit {
+  classesArray: Array<any> = [];
+  newClass: any = {};
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  addNewClass() {
+    this.classesArray.push(this.newClass);
+    this.newClass = {};
   }
 
+  deleteClass(index) {
+    this.classesArray.splice(index);
+  }
 }

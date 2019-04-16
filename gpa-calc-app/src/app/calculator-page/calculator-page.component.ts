227af2fed94a2b2fdfdd20b350516bb65cf00cea
Harjas Monga <harjasmonga@gmail.com>
2019-04-15 23:06:46 -0400

    basic setup for add and delete

--- gpa-calc-app/src/app/calculator-page/calculator-page.component.ts
+++ gpa-calc-app/src/app/calculator-page/calculator-page.component.ts
@@ -8,17 +8,21 @@ import { Component, OnInit } from "@angular/core";
 export class CalculatorPageComponent implements OnInit {
   classesArray: Array<any> = [];
   newClass: any = {};
+  newClassGrade: string;
+  newClassName: string;
 
   constructor() {}
 
   ngOnInit() {}
 
   addNewClass() {
-    this.classesArray.push(this.newClass);
+    this.classesArray.push({"name": this.newClassName, "grade": this.newClassGrade});
     this.newClass = {};
+    this.newClassGrade = "";
+    this.newClassName = "";
   }
 
   deleteClass(index) {
-    this.classesArray.splice(index);
+    this.classesArray.splice(index, 1);
   }
 }

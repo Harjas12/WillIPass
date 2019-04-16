b63995ad53082bc6f63f985ad8ca424793c9053e
jharesco <jharesco@purdue.edu>
2019-04-16 18:49:08 -0400

    fixed typo

--- gpa-calc-app/src/app/app.module.ts
+++ gpa-calc-app/src/app/app.module.ts
@@ -2,25 +2,24 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 
-import { CommonModule } from '@angular/common';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { CommonModule } from "@angular/common";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
 
 //Pages
-import { RegisterComponent } from './register/register.component';
+import { RegisterComponent } from "./register/register.component";
 import { LoginComponent } from "./login/login.component";
 import { HomeComponent } from "./home/home.component";
 import { CalculatorPageComponent } from "./calculator-page/calculator-page.component";
-
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    CalculatorPageComponent
-    RegisterComponent,
+    CalculatorPageComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,

de036266094dbfa63467969f28d2c8cd914906eb
jharesco <jharesco@purdue.edu>
2019-04-16 21:05:06 -0400

    added CSS for login page

--- gpa-calc-app/src/app/app.module.ts
+++ gpa-calc-app/src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { CustomMaterialModule } from "./material.module";
@@ -24,6 +26,7 @@ import { CalculatorPageComponent } from "./calculator-page/calculator-page.compo
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
     CommonModule,
